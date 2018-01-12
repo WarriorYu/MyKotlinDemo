1b982b6a18efed72e0f9c6fd078c894b424693a1
yxb <soldieryu.dev@gmail.com>
2018-01-12 16:30:20 +0800

    自定义组合控件

--- app/src/main/java/com/example/yuxibing/mykotlindemo/javacode/activity/MainActivity.java
+++ app/src/main/java/com/example/yuxibing/mykotlindemo/javacode/activity/MainActivity.java
@@ -1,9 +1,22 @@
 package com.example.yuxibing.mykotlindemo.javacode.activity;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.example.yuxibing.mykotlindemo.R;
 
@@ -14,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setTransparentBar(200, 0);
         //饼状图
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,8 +60,113 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), SettingActivity.class));
             }
         });
+    }
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public void setTransparentBar(@ColorInt int color, int alpha) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            View decorView = window.getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+
+            int finalColor = alpha == 0 ? Color.TRANSPARENT :
+                    Color.argb(alpha, Color.red(color), Color.green(color), Color.blue(color));
+
+//            window.setNavigationBarColor(finalColor);
+            window.setStatusBarColor(finalColor);
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            ViewGroup decorView = (ViewGroup) window.getDecorView();
+            int finalColor = alpha == 0 ? Color.TRANSPARENT : Color.argb(alpha, Color.red(color), Color.green(color), Color.blue(color));
+            decorView.addView(createStatusBarView(this, finalColor));
+            /*if (navigationBarExist(this)) {
+                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+                decorView.addView(createNavBarView(this, finalColor));
+            }*/
+        }
 
     }
 
+    private View createStatusBarView(Context context, @ColorInt int color) {
+        View mStatusBarTintView = new View(context);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams
+                (FrameLayout.LayoutParams.MATCH_PARENT, getStatusBarHeight(context));
+        params.gravity = Gravity.TOP;
+        mStatusBarTintView.setLayoutParams(params);
+        mStatusBarTintView.setBackgroundColor(color);
+        return mStatusBarTintView;
+    }
+
+    private View createNavBarView(Context context, @ColorInt int color) {
+        View mNavBarTintView = new View(context);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams
+                (FrameLayout.LayoutParams.MATCH_PARENT, getNavigationHeight(context));
+        params.gravity = Gravity.BOTTOM;
+        mNavBarTintView.setLayoutParams(params);
+        mNavBarTintView.setBackgroundColor(color);
+        return mNavBarTintView;
+    }
+
+
+    private boolean navigationBarExist(Activity activity) {
+        WindowManager windowManager = activity.getWindowManager();
+        Display d = windowManager.getDefaultDisplay();
+
+        DisplayMetrics realDisplayMetrics = new DisplayMetrics();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            d.getRealMetrics(realDisplayMetrics);
+        }
+
+        int realHeight = realDisplayMetrics.heightPixels;
+        int realWidth = realDisplayMetrics.widthPixels;
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        d.getMetrics(displayMetrics);
+
+        int displayHeight = displayMetrics.heightPixels;
+        int displayWidth = displayMetrics.widthPixels;
+
+        return (realWidth - displayWidth) > 0 || (realHeight - displayHeight) > 0;
+    }
+
+
+    @ColorInt
+    private int calculateColor(@ColorInt int color, int alpha) {
+        float a = 1 - alpha / 255f;
+        int red = color >> 16 & 0xff;
+        int green = color >> 8 & 0xff;
+        int blue = color & 0xff;
+        red = (int) (red * a + 0.5);
+        green = (int) (green * a + 0.5);
+        blue = (int) (blue * a + 0.5);
+        return 0xff << 24 | red << 16 | green << 8 | blue;
+    }
+
+
+    private void setRootView(Activity activity, boolean fit) {
+        ViewGroup parent = (ViewGroup) activity.findViewById(android.R.id.content);
+        for (int i = 0, count = parent.getChildCount(); i < count; i++) {
+            View childView = parent.getChildAt(i);
+            if (childView instanceof ViewGroup) {
+                childView.setFitsSystemWindows(fit);
+                ((ViewGroup) childView).setClipToPadding(fit);
+            }
+        }
+    }
+
+
+    private int getStatusBarHeight(Context context) {
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        return context.getResources().getDimensionPixelSize(resourceId);
+    }
+
+    public int getNavigationHeight(Context context) {
+
+        int resourceId = context.getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        return context.getResources().getDimensionPixelSize(resourceId);
+    }
 }
--- app/src/main/java/com/example/yuxibing/mykotlindemo/kotlincode/MainActivity.kt
+++ app/src/main/java/com/example/yuxibing/mykotlindemo/kotlincode/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.yuxibing.mykotlindemo.kotlincode
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.example.yuxibing.mykotlindemo.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,18 +14,24 @@ class MainActivity : AppCompatActivity() {
         for ((index, value) in array.withIndex()) {
             println("the element at $index is $value")
         }
-        joinOptions(listOf("ues","no","maybe"))
-
+        joinOptions(listOf("ues", "no", "maybe"))
+        val listWithNulls: List<String?> = listOf("A", null)
+        for (item in listWithNulls) {
+            item?.let {
+                Log.e("空类型", it)
+            }
+        }
     }
 
-    fun maxof(a: Int, b: Int) : Int{
+    fun maxof(a: Int, b: Int): Int {
         if (a > b) {
             return a
         } else {
             return b
         }
     }
-    fun minof(a:Int,b: Int) = if (a>b) a else b
-    fun joinOptions(options : Collection<String>) = options.joinToString(prefix = "[",postfix = "]")
+
+    fun minof(a: Int, b: Int) = if (a > b) a else b
+    fun joinOptions(options: Collection<String>) = options.joinToString(prefix = "[", postfix = "]")
 
 }
